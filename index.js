f064861ccd8c7241b6bf9eb5cd7757fcc2bdec0f
Ralph Largo <devlargs@gmail.com>
2020-09-27 18:17:57 +0800

    fix indentation

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 var Bacon = React.createClass({
-		render: function() {
-			return (
-			<div>
-			<h2>A sample component!</h2>
-			<p>Hey there</p>
-			</div>
-			);
-		}
+  render: function () {
+    return (
+      <div>
+        <h2>A sample component!</h2>
+        <p>Hey there</p>
+      </div>
+    );
+  },
 });
-
-
 
 var Movie = React.createClass({
-		render: function() {
-			return (
-				<div>
-					<h1>{this.props.title}</h1>
-					<h2>{this.props.genre}</h2>
-				</div>
-			)
-		}
+  render: function () {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.genre}</h2>
+      </div>
+    );
+  },
 });
 
-	ReactDOM.render(<div>
-		
-			<Movie title="Captain America" genre="Action"/>
-			<Movie title="Breaking Bad" genre="Thriller"/>
-			<Movie title="The Notebook" genre="Romance"/>
-
-	</div>, document.getElementById('example'));
+ReactDOM.render(
+  <div>
+    <Movie title="Captain America" genre="Action" />
+    <Movie title="Breaking Bad" genre="Thriller" />
+    <Movie title="The Notebook" genre="Romance" />
+  </div>,
+  document.getElementById("example")
+);
